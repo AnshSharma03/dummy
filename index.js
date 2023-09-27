543e88275885a21893b571feb06bbd4bbee70e8c
“AnuragParashar2000” <anuragparashar03@gmail.com>
2023-09-28 05:28:11 +0530

    changes

--- index.js
+++ index.js
@@ -10,3 +10,8 @@ let get_func_inner = foo();
 console.log(get_func_inner());
 console.log(get_func_inner());
 console.log(get_func_inner());
+console.log(get_func_inner());
+console.log(get_func_inner());
+console.log(get_func_inner());
+console.log(get_func_inner());
+console.log(get_func_inner());
